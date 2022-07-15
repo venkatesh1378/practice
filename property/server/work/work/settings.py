82a2e6a7a2545b1dc5ecc7f9fa4ef970e2a7b1eb
venkatesh1378 <103771081+venkatesh1378@users.noreply.github.com>
2022-07-15 11:52:47 +0530

    Update settings.py

--- property/server/work/work/settings.py
+++ property/server/work/work/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w)^q363-(w4$)l9c=-fm@)a50%&9bv=e_3=6l%83+thu3pw_(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','firstone-11.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','ronchon-choucroute-71042.herokuapp.com']
 
 
 # Application definition
